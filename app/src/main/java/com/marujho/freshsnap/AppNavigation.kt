e88340516fffef91cf6380157eb1f292001424fb
Ruben-am <ru.alba.dev@gmail.com>
2026-01-08 09:43:51 +0100

    implement navigation logic between screens and refactor classes correctly

--- app/src/main/java/com/marujho/freshsnap/AppNavigation.kt
+++ app/src/main/java/com/marujho/freshsnap/AppNavigation.kt
@@ -1,60 +1,20 @@
 package com.marujho.freshsnap
 
-import android.graphics.Picture
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(
-                    painter = painterResource(R.drawable.ic_freshsnap_logo),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(128.dp)
-                )
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.displayMedium,
-                    modifier = Modifier
-                        .paddingFromBaseline(top = 64.dp)
-                )
-            }
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController)
+        }
+        composable("main_screen") {
+            MainScreen()
         }
     }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun SplashScreenPreview() {
-    SplashScreen()
 }
