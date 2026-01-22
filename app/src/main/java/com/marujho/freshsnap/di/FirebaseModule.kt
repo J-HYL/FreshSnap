46450e8ac85f645ce09ebea64457119aba08bcad
Ruben-am <ru.alba.dev@gmail.com>
2026-01-22 12:09:19 +0100

    create firebase module

---
+++ app/src/main/java/com/marujho/freshsnap/di/FirebaseModule.kt
@@ -0,0 +1,24 @@
+package com.marujho.freshsnap.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+// Usamos hilt para insertar una sola vez la base de datos para toda la app
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+}
