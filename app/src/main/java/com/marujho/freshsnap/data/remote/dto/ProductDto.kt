48c27708a26962258cbbe2ad1dc95ec704936aea
Jhon Hau Yon <jhonrodrigohyl@gmail.com>
2026-01-29 13:11:18 +0100

    Cambio ProductDto, cambio en que devulve la API OFF

--- app/src/main/java/com/marujho/freshsnap/data/remote/dto/ProductDto.kt
+++ app/src/main/java/com/marujho/freshsnap/data/remote/dto/ProductDto.kt
@@ -9,5 +9,24 @@ data class ProductDto(
     val brands: String?,
 
     @Json(name = "image_front_url")
-    val imageUrl: String?
+    val imageUrl: String?,
+
+    val quantity: String?,
+
+    val categories: String?,
+
+    val packaging: String?,
+
+    val countries: String?,
+
+    @Json(name = "nutriscore_grade")
+    val nutriScore: String?,
+
+    @Json(name = "nova_group")
+    val novaGroup: Int?,
+
+    @Json(name = "ecoscore_grade")
+    val greenScore: String?
+
+
 )
