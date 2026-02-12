056b9ae4f2b8e645e5457b1a3fe2f9e9f97b59a2
Jhon Hau Yon <jhonrodrigohyl@gmail.com>
2026-02-12 12:20:48 +0100

    Switch Dark and Light mode

--- app/src/main/java/com/marujho/freshsnap/MainActivity.kt
+++ app/src/main/java/com/marujho/freshsnap/MainActivity.kt
@@ -8,8 +8,12 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.marujho.freshsnap.ui.openFoodFacts.OpenFoodViewModel
+import com.marujho.freshsnap.ui.settings.SettingsViewModel
 import com.marujho.freshsnap.ui.theme.FreshSnapTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +27,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            FreshSnapTheme {
+            val settingsViewModel: SettingsViewModel = viewModel()
+            val isDarkMode by settingsViewModel.isDarkMode.collectAsState()
+
+            FreshSnapTheme(darkTheme = isDarkMode){
                 Surface(
                     modifier = Modifier.Companion.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
